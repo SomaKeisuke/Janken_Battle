23fc70925a53adf807439e955dc2e83e55df4bb6
Nishioka Yoshihiko <nishioka_y@worksap.co.jp>
2016-03-16 17:06:04 +0900

    じゃんけん作成

--- src/janken_battle/players/YourCode.java
+++ src/janken_battle/players/YourCode.java
@@ -8,7 +8,9 @@ import janken_battle.Main.JankenPlayer;
 /**
  * あなたです。
  * TODO アルゴリズムの説明や工夫ポイントがあればここに記入してください。
- *
+ * チョキを積極的に出してポイントを稼ぐタイプです。
+ * 相手がこっちの手を見て二連続グー出してきたらパーを出して回避します。
+ * さらに、相手が1,2回目にいきなりチョキ連発時にはグーを出して稼いでおき、その後上記のパターンに戻して勝ちにいきます。
  */
 public class YourCode implements JankenPlayer {
 
@@ -16,24 +18,33 @@ public class YourCode implements JankenPlayer {
     public String getName() {
         // TODO
         // プレイヤーの名前を半角英数で入力してください。
-        return "YourCode";
+        return "Nishioka Yoshihiko";
     }
 
     @Override
-    public Hand getHand(List<Hand> myHands, List<Hand> enemyHands,int myPoint ,int enemyPoint ,int gameNumber) {
+    public Hand getHand(List<Hand> myHands, List<Hand> enemyHands, int myPoint, int enemyPoint, int gameNumber) {
         // TODO
         // 今は例として、最初の1回はグー、1つ前に相手が出した手がチョキならパー、
         // それ以外ならチョキを出すように実装されています。
         // このメソッドの実装を変更して勝利を目指してください。
         if (myHands.isEmpty()) {
-            return Hand.G;
+            return Hand.C;
         } else {
-            Hand beforeEnemyHand = enemyHands.get(enemyHands.size() - 1);
-            if (beforeEnemyHand == Hand.C) {
-                return Hand.P;
-            } else {
-                return Hand.C;
+            if (enemyHands.size() >= 2) {
+                Hand beforeEnemyHand = enemyHands.get(enemyHands.size()
+                        - 1);
+                Hand before2EnemyHand = enemyHands.get(enemyHands.size()
+                        - 2);
+                if (before2EnemyHand == Hand.G
+                        && beforeEnemyHand == Hand.G) {
+                    return Hand.P;
+                } else {
+                    return Hand.C;
+                }
+            } else if (myPoint == enemyPoint) {
+                return Hand.G;
             }
         }
+        return Hand.C;
     }
 }
