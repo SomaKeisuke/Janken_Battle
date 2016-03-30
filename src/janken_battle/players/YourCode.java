c26f287d5ae7490417b10c519754b283a148dd1a
Nishioka Yoshihiko <nishioka_y@worksap.co.jp>
2016-03-30 17:22:34 +0900

    パワーアップ

--- src/janken_battle/players/YourCode.java
+++ src/janken_battle/players/YourCode.java
@@ -8,9 +8,17 @@ import janken_battle.Main.JankenPlayer;
 /**
  * あなたです。
  * TODO アルゴリズムの説明や工夫ポイントがあればここに記入してください。
- * チョキを積極的に出してポイントを稼ぐタイプです。
- * 相手がこっちの手を見て二連続グー出してきたらパーを出して回避します。
- * さらに、相手が1,2回目にいきなりチョキ連発時にはグーを出して稼いでおき、その後上記のパターンに戻して勝ちにいきます。
+ * 
+ * 試合の状況で手のパターンを変えます。
+ * 
+ * 勝っているとき
+ * 相手の一つ前の手を見て、それに勝つ手を出します。
+ * 
+ * 負けているとき
+ * チョキを積極的に出してポイントを稼ぎにいくタイプです。
+ * 相手が二連続グーを出してきたらパーを出すことで、こちらの手が読まれるのを回避します。
+ * 
+ * さらに、相手が初手でいきなりチョキを出してきたときは、グーを出して稼いでおきます。
  */
 public class YourCode implements JankenPlayer {
 
@@ -30,7 +38,19 @@ public class YourCode implements JankenPlayer {
         if (myHands.isEmpty()) {
             return Hand.C;
         } else {
-            if (enemyHands.size() >= 2) {
+            
+            if (enemyHands.size() > 2 && myPoint > enemyPoint) {
+                Hand beforeEnemyHand = enemyHands.get(enemyHands.size()
+                        - 1);
+                    switch (beforeEnemyHand) {
+                        case G:
+                            return Hand.P;
+                        case P:
+                            return Hand.C;
+                        case C:
+                            return Hand.G;
+                    }
+            } else if (enemyHands.size() > 2 && myPoint <= enemyPoint) {
                 Hand beforeEnemyHand = enemyHands.get(enemyHands.size()
                         - 1);
                 Hand before2EnemyHand = enemyHands.get(enemyHands.size()
@@ -44,7 +64,7 @@ public class YourCode implements JankenPlayer {
             } else if (myPoint == enemyPoint) {
                 return Hand.G;
             }
+            return Hand.C;
         }
-        return Hand.C;
-    }
+    }    
 }
